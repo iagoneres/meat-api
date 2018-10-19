dc3d99712e9a4cbb0db16ec01cb5397100a74691
Iago <iagoneresb@gmail.com>
2018-10-18 23:37:53 -0300

    Create enviroment

--- dist/server/server.js
+++ dist/server/server.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const restify = require("restify");
 class Server {
-    initRoutes() {
+    initRoutes(routers) {
         return new Promise((resolve, reject) => {
             try {
                 this.application = restify.createServer({
@@ -11,31 +11,9 @@ class Server {
                 });
                 this.application.use(restify.plugins.queryParser());
                 //routes
-                this.application.get('/hello', (req, resp, next) => {
-                    // resp.contentType = 'application/json';
-                    resp.status(400);
-                    resp.setHeader('Content-Type', 'application/json');
-                    resp.json({ message: 'hello' });
-                    return next();
-                });
-                this.application.get('/info', (req, resp, next) => {
-                    if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
-                        // resp.status(400);
-                        // resp.json({message: 'Please, update your browser'});
-                        let error = new Error();
-                        error.statusCode = 400;
-                        error.message = 'Please, update your browser';
-                        return next(false);
-                    }
-                    resp.json({
-                        browser: req.userAgent(),
-                        method: req.method,
-                        url: req.href(),
-                        path: req.path(),
-                        query: req.query
-                    });
-                    return next();
-                });
+                for (let router of routers) {
+                    router.applyRoutes(this.application);
+                }
                 this.application.listen(3000, () => {
                     resolve(this.application);
                 });
@@ -45,8 +23,8 @@ class Server {
             }
         });
     }
-    bootstrap() {
-        return this.initRoutes().then(() => this);
+    bootstrap(routers = []) {
+        return this.initRoutes(routers).then(() => this);
     }
 }
 exports.Server = Server;
